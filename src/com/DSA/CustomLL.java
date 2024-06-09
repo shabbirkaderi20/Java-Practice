0774cb455a652a7507aebc6b7a670cb666a3d2fc
shabbirkaderi20 <82316061+shabbirkaderi20@users.noreply.github.com>
2024-06-09 20:34:00 +0530

    Add and show method for double-sided linkedlist

--- src/com/DSA/CustomLL.java
+++ src/com/DSA/CustomLL.java
@@ -4,92 +4,93 @@ public class CustomLL {
 
 	public static void main(String[] args) {
 		
-		LinkedList list= new LinkedList();
-		
-		list.add(1);
-		list.add(2);
-		list.add(3);
-		list.add(4);
-		
+		LinkedList<Companies> list= new LinkedList<>();
+		list.add(new Companies(1L, "CG", "EPIP Phase- 2"));
+		list.add(new Companies(2L, "IBM", "EGL Kormangala"));
 		list.show();
-
 	}
 
 }
 
-class LinkedList {
-	
+class LinkedList<T> {
+
 	Node head;
-	
-	public void add(int data) {
-		
-		Node node= new Node();
-		node.data= data;
-		node.next= null;
-		
-		if(head== null) {
+	Node tail;
+
+	public void add(T data) {
+		Node<T> node = new Node<>(data);
+		if(head== null && tail== null) {
 			head= node;
+			tail= node;
 		}else {
-			Node n= head;
-				
-			while(n.next!= null) {
-				n= n.next;
-			}
-			n.next= node;
+			tail.next= node;
+			tail= node;
 		}
 	}
-	
+
 	public void show() {
-		
 		Node node= head;
-		
-		do{
+		while(node!= tail) {
 			System.out.println(node.data);
 			node= node.next;
-		}while(node.next!= null);
-		
-		System.out.println(node.data);
+		}
+		System.out.println(tail.data);
 	}
 	
-	private static class Node {
-		int data;
-		Node next;	
+	private static class Node<T> {
+
+		Node prev;
+		Node next;
+		T data;
+
+		Node(T data) {
+			this.data =data;
+		}
+
 	}
 }
 
-//class LinkedList {
-//	
-//	private static class Node {
-//		
-//		int data;
-//		Node next;
-//		
-//		public Node(int data) {
-//			
-//			this.data= data;
-//			next= null;
-//		}
-//		
-//	}
-//	
-//	Node head;
-//	
-//	public void add(int node) {
-//		
-//		Node toInsert= new Node(node);
-//		
-//		if(head== null) {
-//			head= toInsert;
-//			return;
-//		}else {
-//			Node n= head;
-//			
-//			while(n.next!= null) {
-//				n= n.next;
-//			}
-//			n.next= toInsert;
-//		}
-//	}
-//	
-//	public static 
-//}
+class Companies {
+
+	private Long companyId;
+	private String companyName;
+	private String companyAddress;
+
+	public Companies(Long companyId, String companyName, String companyAddress) {
+		this.companyId = companyId;
+		this.companyName = companyName;
+		this.companyAddress = companyAddress;
+	}
+
+	public Long getCompanyId() {
+		return companyId;
+	}
+
+	public void setCompanyId(Long companyId) {
+		this.companyId = companyId;
+	}
+
+	public String getCompanyName() {
+		return companyName;
+	}
+
+	public void setCompanyName(String companyName) {
+		this.companyName = companyName;
+	}
+
+	public String getCompanyAddress() {
+		return companyAddress;
+	}
+
+	public void setCompanyAddress(String companyAddress) {
+		this.companyAddress = companyAddress;
+	}
+
+	@Override
+	public String toString() {
+		return "{companyId=" + companyId +
+				", companyName='" + companyName + '\'' +
+				", companyAddress='" + companyAddress + '\'' +
+				'}';
+	}
+}
